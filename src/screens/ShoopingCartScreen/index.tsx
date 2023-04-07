0530b9b3d9abb3a193209413f8e2f46387f09644
dev.mspl <developer.mspl@gmail.com>
2023-04-07 11:15:50 +0530

    ...

--- src/screens/ShoopingCartScreen/index.tsx
+++ src/screens/ShoopingCartScreen/index.tsx
@@ -3,7 +3,14 @@
 import {useNavigation} from '@react-navigation/native';
 import {compose} from '@reduxjs/toolkit';
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View, Image,SafeAreaView} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  SafeAreaView,
+} from 'react-native';
 
 import {useDispatch, useSelector} from 'react-redux';
 import ActionBtn from '../../components/ActionBtn';
@@ -96,171 +103,173 @@ const Cart = () => {
   };
 
   return (
-    <SafeAreaView style={{flex:1, backgroundColor: 'white'}}>
-    <View style={styles.page}>
-      <Text style={styles.cart}>Cart</Text>
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <View style={styles.page}>
+        <Text style={styles.cart}>Cart</Text>
 
-      {countryViseProductsArray.length ? (
-        <>
-          <Text
-            style={{
-              fontSize: 20,
+        {countryViseProductsArray.length ? (
+          <>
+            <Text
+              style={{
+                fontSize: 20,
 
-              color: 'black',
-              marginBottom: 10,
-              marginLeft: 27,
-            }}>
-            Cart By Categories
-          </Text>
-          <ScrollView style={{paddingHorizontal: 25}}>
-            <Accordion
-              underlayColor="transparent"
-              sections={countryViseProductsArray}
-              activeSections={activeSections}
-              renderHeader={(section, _, isActive) => {
-                const total = section.content?.reduce((total, item) => {
-                  // let cost = item?.content?.cost?.replaceAll(' ', '')?.slice(1);
-                  let price = item?.price?.replaceAll(' ', '')?.slice(1);
-                  price = Number(price) * item.qty;
-                  let newTotal = total + price;
-                  // console.log(`total ${total} + ${cost} = ${newTotal}`);
-                  return newTotal;
-                }, 0);
-                return (
-                  <View
-                    style={{
-                      backgroundColor: isActive ? '#d2d2d2' : 'white',
-                      elevation: 1,
-                      borderRadius: 5,
-                      padding: 15,
-                      marginVertical: 10,
-                    }}>
-                    <TouchableOpacity
-                      onPress={() => handleCrossPress(section.content)}
-                      style={{
-                        backgroundColor: 'white',
-                        width: 20,
-                        height: 20,
-                        borderRadius: 20,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        position: 'absolute',
-                        top: 5,
-                        right: 5,
-                        zIndex: 1,
-                      }}>
-                      <Ionicons name="close" size={15} color="black" />
-                    </TouchableOpacity>
-                    <Text
-                      style={{
-                        fontSize: 16,
-                        fontWeight: '900',
-                      }}>
-                      {section.title}
-                    </Text>
-
+                color: 'black',
+                marginBottom: 10,
+                marginLeft: 27,
+              }}>
+              Cart By Categories
+            </Text>
+            <ScrollView style={{paddingHorizontal: 25}}>
+              <Accordion
+                underlayColor="transparent"
+                sections={countryViseProductsArray}
+                activeSections={activeSections}
+                renderHeader={(section, _, isActive) => {
+                  const total = section.content?.reduce((total, item) => {
+                    // let cost = item?.content?.cost?.replaceAll(' ', '')?.slice(1);
+                    let price = item?.price?.replaceAll(' ', '')?.slice(1);
+                    price = Number(price) * item.qty;
+                    let newTotal = total + price;
+                    // console.log(`total ${total} + ${cost} = ${newTotal}`);
+                    return newTotal;
+                  }, 0);
+                  return (
                     <View
                       style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        paddingVertical: 12,
+                        backgroundColor: isActive ? '#d2d2d2' : 'white',
+                        elevation: 1,
+                        borderRadius: 5,
+                        padding: 15,
+                        marginVertical: 10,
                       }}>
-                      <View style={{flexDirection: 'row'}}>
-                        <Text
-                          style={{
-                            fontWeight: 'bold',
-                            fontSize: 16,
-                            color: 'black',
-                          }}>
-                          Total:{' '}
-                        </Text>
-                        <Text
-                          style={{
-                            fontWeight: 'bold',
-                            fontSize: 18,
-                            color: 'black',
-                          }}>
-                          $ {total?.toFixed(2)}
-                        </Text>
-                      </View>
-                      <View style={{flexDirection: 'row'}}>
-                        <Text
-                          style={{
-                            fontWeight: 'bold',
-                            fontSize: 16,
-                            color: 'black',
-                          }}>
-                          Net Qty:{' '}
-                        </Text>
-                        <Text
-                          style={{
-                            fontWeight: 'bold',
-                            fontSize: 19,
-                            color: 'black',
-                          }}>
-                          {section.content.length}
-                        </Text>
+                      <TouchableOpacity
+                        onPress={() => handleCrossPress(section.content)}
+                        style={{
+                          backgroundColor: 'white',
+                          width: 20,
+                          height: 20,
+                          borderRadius: 20,
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          position: 'absolute',
+                          top: 5,
+                          right: 5,
+                          zIndex: 1,
+                        }}>
+                        <Ionicons name="close" size={15} color="black" />
+                      </TouchableOpacity>
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          fontWeight: '900',
+                        }}>
+                        {section.title}
+                      </Text>
+
+                      <View
+                        style={{
+                          flexDirection: 'row',
+                          justifyContent: 'space-between',
+                          paddingVertical: 12,
+                        }}>
+                        <View style={{flexDirection: 'row'}}>
+                          <Text
+                            style={{
+                              fontWeight: 'bold',
+                              fontSize: 16,
+                              color: 'black',
+                            }}>
+                            Total:{' '}
+                          </Text>
+                          <Text
+                            style={{
+                              fontWeight: 'bold',
+                              fontSize: 18,
+                              color: 'black',
+                            }}>
+                            $ {total?.toFixed(2)}
+                          </Text>
+                        </View>
+                        <View style={{flexDirection: 'row'}}>
+                          <Text
+                            style={{
+                              fontWeight: 'bold',
+                              fontSize: 16,
+                              color: 'black',
+                            }}>
+                            Net Qty:{' '}
+                          </Text>
+                          <Text
+                            style={{
+                              fontWeight: 'bold',
+                              fontSize: 19,
+                              color: 'black',
+                            }}>
+                            {section.content.length}
+                          </Text>
+                        </View>
                       </View>
                     </View>
-                  </View>
-                );
-              }}
-              renderContent={section => {
-                return (
-                  <View style={{paddingBottom: 20}}>
-                    {section?.content?.map(item => (
-                      <CartItem cartItem={item} />
-                    ))}
-                    <View style={{alignItems: 'center'}}>
-                      <ActionBtn
-                        title={'checkout now!'}
-                        containerStyle={{width: '90%', borderRadius: 5}}
-                        onPress={() => {
-                          navigation.navigate('Address', {
-                            cartItemData: section?.content,
-                          });
-                        }}
-                      />
+                  );
+                }}
+                renderContent={section => {
+                  return (
+                    <View style={{paddingBottom: 20}}>
+                      {section?.content?.map(item => (
+                        <CartItem cartItem={item} />
+                      ))}
+                      <View style={{alignItems: 'center'}}>
+                        <ActionBtn
+                          title={'checkout now!'}
+                          containerStyle={{width: '90%', borderRadius: 5}}
+                          onPress={() => {
+                            navigation.navigate('Address', {
+                              cartItemData: section?.content,
+                            });
+                          }}
+                        />
+                      </View>
                     </View>
-                  </View>
-                );
-              }}
-              onChange={s => {
-                setActiveSections(s);
+                  );
+                }}
+                onChange={s => {
+                  setActiveSections(s);
+                }}
+              />
+            </ScrollView>
+          </>
+        ) : (
+          <View
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+              source={require('../../icons/empty.png')}
+              style={{width: 200, height: 200, transform: [{translateX: -18}]}}
+              resizeMode="contain"
+            />
+            <Text
+              style={{
+                fontWeight: 'bold',
+                fontSize: 16,
+                marginTop: 20,
+                opacity: 0.4,
+              }}>
+              Cart Is Empty!
+            </Text>
+          </View>
+        )}
+        {countryViseProductsArray.length ? (
+          <View style={{alignItems: 'center', padding: 10}}>
+            <BigCheckoutBtn
+              title={'All Carts Checkout                         '}
+              total={`$ ${cartTotal.toFixed(2)}`}
+              onPress={() => {
+                navigation.navigate('Address', {cartItemData: cartItems});
               }}
             />
-          </ScrollView>
-        </>
-      ) : (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <Image
-            source={require('../../icons/empty.png')}
-            style={{width: 200, height: 200, transform: [{translateX: -18}]}}
-            resizeMode="contain"
-          />
-          <Text
-            style={{
-              fontWeight: 'bold',
-              fontSize: 16,
-              marginTop: 20,
-              opacity: 0.4,
-            }}>
-            Cart Is Empty!
-          </Text>
-        </View>
-      )}
-      {countryViseProductsArray.length ? (
-        <View style={{alignItems: 'center', padding: 10}}>
-          <BigCheckoutBtn
-            title={'All Carts Checkout                         '}
-            total={`$ ${cartTotal.toFixed(2)}`}
-            onPress={() => {
-              navigation.navigate('Address', {cartItemData: cartItems});
-            }}
-          />
-        </View>
-      ) : null}
-    </View></SafeAreaView>
+          </View>
+        ) : null}
+      </View>
+    </SafeAreaView>
   );
 };
 
